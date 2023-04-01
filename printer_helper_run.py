89379d71b9073a0bc7c04ffad485d5396aea61f5
AleksandrTurchin <107668514+AleksandrTurchin@users.noreply.github.com>
2023-04-01 19:44:57 +0300

    changed

---
+++ printer_helper_run.py
@@ -0,0 +1,11 @@
+import time
+import os
+
+for i in range(1, 9):
+	os.system('rundll32 printui.dll,PrintUIEntry /k /n "HP LaserJet Professional P1102" ')
+	time.sleep(3480)  # время задержки выполнения команды os.system() в секундах
+
+	t = time.localtime()
+	current_time = time.strftime("%H:%M:%S (%d.%m)", t)
+	print(f'Печать тестовой страницы №{i} выполнено в {current_time}')
+
